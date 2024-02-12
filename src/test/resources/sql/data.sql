e113a8c3abe38c09e4990afe0e00fb5724afe4ea
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2024-02-12 10:36:37 +0100

    333

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -2,14 +2,14 @@ INSERT INTO users (first_name, last_name, email, hash_password, user_role, level
 VALUES ('Ivan', 'Testov', 'ivan.testov@example.com', 'hashedPassword', 'USER', 1),
        ('Lidiya', 'Testova', 'lidiya.testova@example.com', 'hashedPassword', 'USER', 2);
 
-INSERT INTO applicants (first_name, last_name, email, address, phone_number, hash_password)
-VALUES ('John', 'Doe', 'john.doe@example.com', '1234 Main St', '123-456-7890', 'hashed_password_1'),
-       ('Jane', 'Doe', 'jane.doe@example.com', '5678 Market St', '098-765-4321', 'hashed_password_2');
-
-INSERT INTO exam_task (id, exam_task_title)
-VALUES (1, 'Final Exam'), (2, 'Arrays'), (3, 'Cycles');
-
-INSERT INTO exam (exam_score, exam_start_time, exam_end_time, exam_duration, exam_status, user_id, applicant_id, examtask_id)
-VALUES (80, '2023-01-01 10:00:00', '2023-01-01 11:30:00', 70, 'PLANNED', 1, 1, 1),
-       (85, '2023-01-01 10:00:00', '2023-01-01 11:30:00', 90, 'PLANNED', 1, 1, 2);
+-- INSERT INTO applicants (first_name, last_name, email, address, phone_number, hash_password)
+-- VALUES ('John', 'Doe', 'john.doe@example.com', '1234 Main St', '123-456-7890', 'hashed_password_1'),
+--        ('Jane', 'Doe', 'jane.doe@example.com', '5678 Market St', '098-765-4321', 'hashed_password_2');
+--
+-- INSERT INTO exam_task (id, exam_task_title)
+-- VALUES (1, 'Final Exam'), (2, 'Arrays'), (3, 'Cycles');
+--
+-- INSERT INTO exam (exam_score, exam_start_time, exam_end_time, exam_duration, exam_status, user_id, applicant_id, examtask_id)
+-- VALUES (80, '2023-01-01 10:00:00', '2023-01-01 11:30:00', 70, 'PLANNED', 1, 1, 1),
+--        (85, '2023-01-01 10:00:00', '2023-01-01 11:30:00', 90, 'PLANNED', 1, 1, 2);
 
