a97230509681fefc641627bf12f3ef1edd294905
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2024-02-12 12:54:15 +0100

    37

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -5,10 +5,10 @@ VALUES (1, 'Ivan', 'Testov', 'ivan.testov@example.com', 'hashedPassword', 'USER'
 INSERT INTO applicants (first_name, last_name, email, address, phone_number, hash_password)
 VALUES ('John', 'Doe', 'john.doe@example.com', '1234 Main St', '123-456-7890', 'hashed_password_1'),
        ('Jane', 'Doe', 'jane.doe@example.com', '5678 Market St', '098-765-4321', 'hashed_password_2');
---
--- INSERT INTO exam_task (exam_task_title)
--- VALUES ('Final Exam'), ('Arrays'), ('Cycles');
---
+
+INSERT INTO exam_task (exam_task_title)
+VALUES ('Final Exam'), ('Arrays'), ('Cycles');
+
 -- INSERT INTO exam (exam_score, exam_start_time, exam_end_time, exam_duration, exam_status, user_id, applicant_id, examtask_id)
 -- VALUES (80, '2023-01-01 10:00:00', '2023-01-01 11:30:00', 70, 'PLANNED', 1, 1, 1),
 --        (85, '2023-01-01 10:00:00', '2023-01-01 11:30:00', 90, 'PLANNED', 1, 1, 2);
