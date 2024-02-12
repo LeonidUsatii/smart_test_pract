addbdac8548e55296d2e7eebf73dd31a5f0c3b3a
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2024-02-12 12:03:52 +0100

    24

--- src/test/java/de/ait/smarttestit/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/de/ait/smarttestit/controllers/UsersControllerIntegrationTest.java
@@ -73,33 +73,33 @@ class UsersControllerIntegrationTest {
     }
 
 
-//    @Nested
-//    @DisplayName("DELETE /users/{user-id}:")
-//    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//    class DeleteUser {
-//
-//        @Test
-//        @Sql(scripts = {"/sql/schema.sql", "/sql/users.sql"})
-//        void testDeleteUserPositive() throws Exception {
-//
-//            Long existingUserId = 1L;
-//
-//            mockMvc.perform(delete("/api/users/{user-id}", existingUserId)
-//                            .contentType(MediaType.APPLICATION_JSON))
-//                    .andExpect(status().isNoContent());
-//        }
-//
-//        @Test
-//        @Sql(scripts = {"/sql/schema.sql", "/sql/users.sql"})
-//        void testDeleteUserNegative() throws Exception {
-//
-//            Long nonExistentUserId = 999L;
-//
-//            mockMvc.perform(delete("/api/users/{user-id}", nonExistentUserId)
-//                            .contentType(MediaType.APPLICATION_JSON))
-//                    .andExpect(status().isNotFound());
-//        }
-//    }
+    @Nested
+    @DisplayName("DELETE /users/{user-id}:")
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+    class DeleteUser {
+
+        @Test
+        @Sql(scripts = {"/sql/schema.sql", "/sql/users.sql"})
+        void testDeleteUserPositive() throws Exception {
+
+            Long existingUserId = 1L;
+
+            mockMvc.perform(delete("/api/users/{user-id}", existingUserId)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isNoContent());
+        }
+
+        @Test
+        @Sql(scripts = {"/sql/schema.sql", "/sql/users.sql"})
+        void testDeleteUserNegative() throws Exception {
+
+            Long nonExistentUserId = 999L;
+
+            mockMvc.perform(delete("/api/users/{user-id}", nonExistentUserId)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isNotFound());
+        }
+    }
 
 
 //    @Nested
