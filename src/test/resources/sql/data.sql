acd1c036e2fcfcafd08a5ad9a8ea57d74ff2a141
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2024-02-12 12:51:28 +0100

    36

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -2,9 +2,9 @@ INSERT INTO users (id, first_name, last_name, email, hash_password, user_role, l
 VALUES (1, 'Ivan', 'Testov', 'ivan.testov@example.com', 'hashedPassword', 'USER', 1),
        (2, 'Lidiya', 'Testova', 'lidiya.testova@example.com', 'hashedPassword', 'USER', 2);
 
--- INSERT INTO applicants (first_name, last_name, email, address, phone_number, hash_password)
--- VALUES ('John', 'Doe', 'john.doe@example.com', '1234 Main St', '123-456-7890', 'hashed_password_1'),
---        ('Jane', 'Doe', 'jane.doe@example.com', '5678 Market St', '098-765-4321', 'hashed_password_2');
+INSERT INTO applicants (first_name, last_name, email, address, phone_number, hash_password)
+VALUES ('John', 'Doe', 'john.doe@example.com', '1234 Main St', '123-456-7890', 'hashed_password_1'),
+       ('Jane', 'Doe', 'jane.doe@example.com', '5678 Market St', '098-765-4321', 'hashed_password_2');
 --
 -- INSERT INTO exam_task (exam_task_title)
 -- VALUES ('Final Exam'), ('Arrays'), ('Cycles');
