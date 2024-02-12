d33aaf4c9f830a673f157d91e55bd5566754e096
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2024-02-12 12:18:01 +0100

    29

--- src/test/resources/sql/users.sql
+++ src/test/resources/sql/users.sql
@@ -1,3 +1,4 @@
-INSERT INTO users (first_name, last_name, email, hash_password, user_role, level_of_user)
-VALUES ('Ivan', 'Testov', 'ivan.testov@example.com', 'hashedPassword', 'USER', 1),
-('Lidiya', 'Testova', 'lidiya.testova@example.com', 'hashedPassword', 'USER', 2);
+
+INSERT INTO users (id, first_name, last_name, email, hash_password, user_role, level_of_user)
+VALUES (1, 'Ivan', 'Testov', 'ivan.testov@example.com', 'hashedPassword', 'USER', 1),
+(2, 'Lidiya', 'Testova', 'lidiya.testova@example.com', 'hashedPassword', 'USER', 2);
