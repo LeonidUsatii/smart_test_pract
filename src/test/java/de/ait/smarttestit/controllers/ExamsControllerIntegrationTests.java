8be78dd32cec3db0fdeac6c25e85ebc298c08ca2
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2024-02-12 14:55:30 +0100

    63

--- src/test/java/de/ait/smarttestit/controllers/ExamsControllerIntegrationTests.java
+++ src/test/java/de/ait/smarttestit/controllers/ExamsControllerIntegrationTests.java
@@ -38,9 +38,23 @@ public class ExamsControllerIntegrationTests {
 
         @Test
         @Sql(scripts = {"/sql/data.sql"})
-        void testAddExamNegativeInvalidUserId() throws Exception {
+        void testAddExamPositive() throws Exception {
 
-            //assertThat(examTaskRepository.existsById(2L)).isTrue();
+            mockMvc.perform(post("/api/exams")
+                            .param("examScore", "75")
+                            .param("testStartTime", "2023-02-03T12:00")
+                            .param("examEndTime", "2023-02-03T13:00")
+                            .param("examDuration", "110")
+                            .param("examStatus", "PLANNED")
+                            .param("userId", "1")
+                            .param("examTaskId", "3"))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.id").exists());
+        }
+
+        @Test
+        @Sql(scripts = {"/sql/data.sql"})
+        void testAddExamNegativeInvalidUserId() throws Exception {
 
             mockMvc.perform(post("/api/exams")
                             .param("examScore", "70")
@@ -52,25 +66,6 @@ public class ExamsControllerIntegrationTests {
                             .param("examTaskId", "4"))
                     .andExpect(status().isNotFound());
         }
-
-        @Test
-        @Sql(scripts = {"/sql/data.sql"})
-        void testAddExamPositive() throws Exception {
-
-            //assertThat(examTaskRepository.existsById(5L)).isTrue();
-
-            mockMvc.perform(post("/api/exams")
-                            .param("examScore", "70")
-                            .param("testStartTime", "2023-02-03T12:00")
-                            .param("examEndTime", "2023-02-03T13:00")
-                            .param("examDuration", "120")
-                            .param("examStatus", "PLANNED")
-                            .param("userId", "1")
-                            .param("examTaskId", "3"))
-                    .andExpect(status().isCreated())
-                    .andExpect(jsonPath("$.id").exists());
-        }
-
 
     }
 
