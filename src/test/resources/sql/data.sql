ded4da2c65b9978062fa5c82157589296fceefa2
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2024-02-12 13:00:01 +0100

    39

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -11,5 +11,5 @@ VALUES ('Final Exam'), ('Arrays'), ('Cycles');
 
 INSERT INTO exam (exam_score, exam_start_time, exam_end_time, exam_duration, exam_status, user_id, applicant_id, examtask_id)
 VALUES (80, '2023-01-01 10:00:00', '2023-01-01 11:30:00', 70, 'PLANNED', 1, 1, 1),
-       (85, '2023-01-01 10:00:00', '2023-01-01 11:30:00', 90, 'PLANNED', 1, 1, 2);
+       (85, '2023-01-01 10:00:00', '2023-01-01 11:30:00', 90, 'PLANNED', 2, 2, 2);
 
