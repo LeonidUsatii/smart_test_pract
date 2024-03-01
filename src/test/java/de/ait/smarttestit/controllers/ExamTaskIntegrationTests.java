5570401333a58e642531a67ab58da2e420122978
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2024-03-01 12:50:43 +0100

    add

--- src/test/java/de/ait/smarttestit/controllers/ExamTaskIntegrationTests.java
+++ src/test/java/de/ait/smarttestit/controllers/ExamTaskIntegrationTests.java
@@ -8,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
