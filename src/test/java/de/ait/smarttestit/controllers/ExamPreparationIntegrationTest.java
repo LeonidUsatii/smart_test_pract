b53afa833a34ad9a9a470066635b074f4aee2907
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2024-03-02 15:27:07 +0100

    10

--- src/test/java/de/ait/smarttestit/controllers/ExamPreparationIntegrationTest.java
+++ src/test/java/de/ait/smarttestit/controllers/ExamPreparationIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @DisplayName("Endpoint /ExamPreparation:")
 @DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ExamPreparationIntegrationTest {
 
     @Autowired
